5fbf27ea09a9a6b3992d106d8fbbfd817a0ea25c
Amit Maurya <amit.kmaurya00@gmail.com>
2024-10-15 12:28:35 +0530

    Added views, URLS, and templates for categories and products

---
+++ shop/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import category_list, product_list
+
+urlpatterns = [
+    path('categories/', category_list, name='category_list'),
+    path('products/', product_list, name='product_list'),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'shop/category_list.html', {'categories': categories})
+
+def product_list(request):
+    products = Product.objects.filter(available=True)
+    return render(request, 'shop/product_list.html', {'products': products})
